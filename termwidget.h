63c13520cd025a1faedf3251519e89453d401850
n014361 <peili.wang@archermind.com>
2020-01-10 12:59:03 +0800

    chore: add setting function contains font, fontsize, paste selection and cursor set

--- termwidget.h
+++ termwidget.h
@@ -25,7 +25,7 @@ class TermWidgetWrapper : public QWidget
 public:
     TermWidgetWrapper(TermProperties properties, QWidget *parent = nullptr);
 
-    bool isTitleChanged() const;
+    bool    isTitleChanged() const;
     QString title() const;
 
     QString workingDirectory();
@@ -33,12 +33,31 @@ public:
     void sendText(const QString &text);
     void setTerminalOpacity(qreal opacity);
     void setColorScheme(const QString &name);
+    /******** Modify by n014361 wangpeili 2020-01-06: 修改字体      ***********×****/
+    // 修改字体
+    void setTerminalFont(const QString &fontName);
+    // 修改字体大小
+    void setTerminalFontSize(const int fontSize);
+    // 全选
+    void selectAll();
+    // 跳转到下一个命令
+    void skipToNextCommand();
+    // 跳转到前一个命令
+    void skipToPreCommand();
+    // 修改光标形状
+    void setCursorShape(int shape);
+    // 设置光标是否闪烁
+    void setCursorBlinking(bool enable);
+    /********************* Modify by n014361 wangpeili End ************************/
 
 public slots:
     void zoomIn();
     void zoomOut();
     void copyClipboard();
     void pasteClipboard();
+    /******** Modify by n014361 wangpeili 2020-01-10: 粘贴选择内容   ****************/
+    void pasteSelection();
+    /********************* Modify by n014361 wangpeili End ************************/
 
 signals:
     void termRequestSplit(Qt::Orientation ori);
@@ -52,8 +71,8 @@ signals:
 private:
     void initUI();
 
-    TermWidget *m_term;
+    TermWidget * m_term;
     QVBoxLayout *m_layout;
 };
 
-#endif // TERMWIDGET_H
+#endif  // TERMWIDGET_H
