4c0f01c3f805f813f3c61a2d04356651338564ca
m000714 <daizhengwen@uniontech.com>
2020-03-31 10:53:03 +0800

    Type:fix
    Description:Cancel the editing command interface, and the input cursor in the terminal disappears.
    
    RootCause:Cursor focus on list box, not on terminal interface
    
    Solution:Cancel cursor focus on list box

--- views/rightpanel.cpp
+++ views/rightpanel.cpp
@@ -23,9 +23,9 @@ void RightPanel::show()
 {
     QWidget::show();
     QWidget::raise();
-
-    setFocus();
-
+    /******** Modify by m000714 daizhengwen 2020-03-31: 关闭编辑框光标丢失焦点的问题****************/
+    //setFocus();
+    /********************* Modify by m000714 daizhengwen End ************************/
     QRect rect = geometry();
     QRect windowRect = window()->geometry();
     QPropertyAnimation *animation = new QPropertyAnimation(this, "geometry");
