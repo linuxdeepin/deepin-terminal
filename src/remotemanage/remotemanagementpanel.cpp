616cb9f547fef3d809cedaaa17fe73bd8f2ffc49
daizhengwen <daizhengwen@uniontech.com>
2020-07-27 17:03:38 +0800

    fix: [sp3] bug#39775
    Description: 修改设置焦点的方式
    
    Log: 删除远程管理后，输入框和下一个服务器都存在Focus效果
    
    link: https://pms.uniontech.com/zentao/bug-view-39775.html

--- src/remotemanage/remotemanagementpanel.cpp
+++ src/remotemanage/remotemanagementpanel.cpp
@@ -32,8 +32,10 @@ void RemoteManagementPanel::setFocusInPanel()
         m_searchEdit->lineEdit()->setFocus();
     } else if (m_listWidget->isVisible()) {
         // 列表显示
+        /******** Modify by ut000610 daizhengwen 2020-07-27:bug#39775 Begin***************/
         // 将焦点设置在列表里的第一项
         m_listWidget->setCurrentIndex(0);
+        /********************* Modify by ut000610 daizhengwen End ************************/
     } else {
         // 将焦点显示在添加按钮上
         m_pushButton->setFocus();
