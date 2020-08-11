9004d5edd0fa833678ef4a8907c5b040e37852cb
feixiang.ren <renfeixiang@uniontech.com>
2020-08-11 11:40:55 +0800

    chore: codereview views文件夹头部注释
    codereview views文件夹内
    commonpanel
    focusframe
    iconbutton
    itemwidget
    listview
    myiconbutton
    operationconfirmdlg
    rightpanel
    tabbar
    termbasedialog
    termcommandlinkbutton
    terminputdialog
    termwidget.h
    termwidgetpage
    titlebar
    warnningdlg
    文件的头部注释
    
    Log: null
    
    link: null

--- src/views/iconbutton.h
+++ src/views/iconbutton.h
@@ -1,9 +1,9 @@
 /*
  *  Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd
  *
- * Author:     daizhengwen@uniontech.com
+ * Author:     daizhengwen <daizhengwen@uniontech.com>
  *
- * Maintainer: daizhengwen@uniontech.com
+ * Maintainer: daizhengwen <daizhengwen@uniontech.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
