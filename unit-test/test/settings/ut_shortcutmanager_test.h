e823eb7f8e29278c9f508ee9c8ef00abad21a21e
wangliang <wangliang@uniontech.com>
2020-08-19 17:05:23 +0800

    chore: [sp3] 终端单元测试
    pagesearchbar、titlebar、shortcutmanager单元测试
    
    Log: 终端单元测试
    
    link: Null

---
+++ unit-test/test/settings/ut_shortcutmanager_test.h
@@ -0,0 +1,30 @@
+
+#ifndef UT_SHORTCUTMANAGER_TEST_H
+#define UT_SHORTCUTMANAGER_TEST_H
+
+#include "../common/ut_defines.h"
+
+#include <QObject>
+#include <gtest/gtest.h>
+
+class ShortcutManager;
+class UT_ShortcutManager_Test : public ::testing::Test
+{
+public:
+    UT_ShortcutManager_Test();
+
+public:
+    //这里的几个函数都会自动调用
+
+    //用于做一些初始化操作
+    virtual void SetUp();
+
+    //用于做一些清理操作
+    virtual void TearDown();
+
+public:
+    ShortcutManager *m_shortcutManager;
+};
+
+#endif // UT_SHORTCUTMANAGER_TEST_H
+
--- unit-test/test/views/ut_titlebar_test.cpp
+++ unit-test/test/views/ut_titlebar_test.cpp
@@ -2,6 +2,9 @@
 
 #define private public
 #include "titlebar.h"
+#include "tabbar.h"
+#include "mainwindow.h"
+#include "service.h"
 #undef private
 
 //Google GTest 相关头文件
@@ -12,6 +15,10 @@
 #include <QtGui>
 #include <QSignalSpy>
 #include <QDebug>
+#include <QMainWindow>
+#include <DTitlebar>
+
+DWIDGET_USE_NAMESPACE
 
 UT_TitleBar_Test::UT_TitleBar_Test()
 {
@@ -19,22 +26,64 @@ UT_TitleBar_Test::UT_TitleBar_Test()
 
 void UT_TitleBar_Test::SetUp()
 {
+    if (!Service::instance()->property("isServiceInit").toBool())
+    {
+        Service::instance()->init();
+        Service::instance()->setProperty("isServiceInit", true);
+    }
+
+    m_normalTermProperty[QuakeMode] = false;
+    m_normalTermProperty[SingleFlag] = true;
+    m_normalWindow = new NormalWindow(m_normalTermProperty, nullptr);
 }
 
 void UT_TitleBar_Test::TearDown()
 {
+    delete m_normalWindow;
 }
 
 #ifdef UT_TITLEBAR_TEST
 TEST_F(UT_TitleBar_Test, TitleBarTest)
 {
-//    TitleBar titleBar;
-//    titleBar.resize(800, 50);
-//    titleBar.show();
-//    EXPECT_EQ(titleBar.isVisible(), true);
+    m_normalWindow->resize(800, 600);
+    m_normalWindow->show();
+    EXPECT_EQ(m_normalWindow->isVisible(), true);
 
-//#ifdef ENABLE_UI_TEST
-//    QTest::qWait(1000);
-//#endif
+    DTitlebar *dtkTitleBar = m_normalWindow->titlebar();
+    TitleBar *titleBar = qobject_cast<TitleBar*>(dtkTitleBar->customWidget());
+    EXPECT_NE(titleBar, nullptr);
+    EXPECT_EQ(titleBar->isVisible(), true);
+
+    //清空titleBar布局
+    QHBoxLayout *titleBarLayout = titleBar->m_layout;QLayoutItem *child;
+    while ((child = titleBarLayout->takeAt(0)) != nullptr)
+    {
+        if(child->widget())
+        {
+            child->widget()->setParent(nullptr);
+        }
+        delete child;
+    }
+
+    TabBar tabbar;
+    tabbar.resize(800, 50);
+    tabbar.show();
+    EXPECT_EQ(tabbar.isVisible(), true);
+
+    int tabHeight = 36;
+    tabbar.setTabHeight(tabHeight);
+    EXPECT_EQ(tabbar.height(), 36);
+
+    QString tabName = QString("TestTitleBar");
+    QString tabIdentifier = Utils::getRandString().toLower();
+    tabbar.addTab(tabIdentifier, tabName);
+    titleBar->setTabBar(&tabbar);
+
+    int rightSpace = titleBar->rightSpace();
+    EXPECT_EQ(rightSpace, titleBar->m_rightSpace);
+
+#ifdef ENABLE_UI_TEST
+    QTest::qWait(1000);
+#endif
 }
 #endif
