dec3a28c6da910d50b98a1c8d33c05341165426f
wangliang <wangliang@uniontech.com>
2020-08-21 18:02:57 +0800

    chore: [sp3] remote manage unit test
    完善远程服务器管理单元测试
    
    Log: remote manage unit test
    
    link: Null

---
+++ unit-test/test/remotemanage/ut_remotemanagementtoppanel_test.cpp
@@ -0,0 +1,58 @@
+#include "ut_remotemanagementtoppanel_test.h"
+
+#define private public
+#include "remotemanagementtoppanel.h"
+#include "service.h"
+#include "mainwindow.h"
+#undef public
+
+//Google GTest 相关头文件
+#include <gtest/gtest.h>
+
+//Qt单元测试相关头文件
+#include <QTest>
+#include <QtGui>
+
+UT_RemoteManagementTopPanel_Test::UT_RemoteManagementTopPanel_Test()
+{
+}
+
+void UT_RemoteManagementTopPanel_Test::SetUp()
+{
+    if (!Service::instance()->property("isServiceInit").toBool())
+    {
+        Service::instance()->init();
+        Service::instance()->setProperty("isServiceInit", true);
+    }
+
+    m_normalTermProperty[QuakeMode] = false;
+    m_normalTermProperty[SingleFlag] = true;
+    m_normalWindow = new NormalWindow(m_normalTermProperty, nullptr);
+}
+
+void UT_RemoteManagementTopPanel_Test::TearDown()
+{
+    delete m_normalWindow;
+}
+
+//#ifdef UT_REMOTEMANAGEMENTTOPPANEL_TEST
+TEST_F(UT_RemoteManagementTopPanel_Test, RemoteManagementTopPanelTest)
+{
+    m_normalWindow->resize(800, 600);
+    m_normalWindow->show();
+    EXPECT_EQ(m_normalWindow->isVisible(), true);
+
+    m_normalWindow->showPlugin(MainWindow::PLUGIN_TYPE_REMOTEMANAGEMENT);
+
+    RemoteManagementPlugin *remotePlugin = m_normalWindow->findChild<RemoteManagementPlugin *>();
+    RemoteManagementTopPanel *remoteTopPanel = remotePlugin->getRemoteManagementTopPanel();
+    EXPECT_NE(remoteTopPanel, nullptr);
+    EXPECT_EQ(remoteTopPanel->isVisible(), true);
+
+    remoteTopPanel->setFocusInPanel();
+
+#ifdef ENABLE_UI_TEST
+    QTest::qWait(1000);
+#endif
+}
+//#endif
---
+++ unit-test/test/remotemanage/ut_serverconfiggrouppanel_test.h
@@ -0,0 +1,31 @@
+
+#ifndef UT_SERVERCONFIGGROUPPANEL_TEST_H
+#define UT_SERVERCONFIGGROUPPANEL_TEST_H
+
+#include "../common/ut_defines.h"
+#include "termproperties.h"
+#include <gtest/gtest.h>
+
+class MainWindow;
+class UT_ServerConfigGroupPanel_Test : public ::testing::Test
+{
+public:
+    UT_ServerConfigGroupPanel_Test();
+
+public:
+    //这里的几个函数都会自动调用
+
+    //用于做一些初始化操作
+    virtual void SetUp();
+
+    //用于做一些清理操作
+    virtual void TearDown();
+
+public:
+    //普通窗口
+    MainWindow *m_normalWindow = nullptr;
+
+    TermProperties m_normalTermProperty;
+};
+
+#endif // UT_SERVERCONFIGGROUPPANEL_TEST_H
