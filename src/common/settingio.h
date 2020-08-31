14ba68c4a93289757b26d7d22600f5e944335b7b
kewei.zhu <kewei.zhu@deepin.io>
2020-08-31 19:43:57 +0800

    chore: [sp2] 解决社区版自定义和远程管理配置文件兼容乱码问题
    解决社区版自定义和远程管理配置文件兼容乱码问题
    
    Log: 解决社区版自定义和远程管理配置文件兼容乱码问题
    
    link: Null

---
+++ src/common/settingio.h
@@ -0,0 +1,27 @@
+#ifndef SETTINGIO_H
+#define SETTINGIO_H
+
+#include <QObject>
+#include <QSettings>
+#include <QIODevice>
+
+class SettingIO : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SettingIO(QObject *parent = nullptr);
+    static bool readIniFunc(QIODevice &device,  QSettings::SettingsMap &settingsMap);
+    static bool writeIniFunc(QIODevice &device, const QSettings::SettingsMap &settingsMap);
+    static QString variantToString(const QVariant &v);
+    static QVariant stringToVariant(const QString &s);
+    static QByteArray escapedString(const QString &src);
+    static QString unescapedString(const QString &src);
+    static QString canTransfer(const QString& str);
+    static bool iniUnescapedKey(const QByteArray &key, int from, int to, QString &result);
+    static bool rewrite;
+signals:
+
+public slots:
+};
+
+#endif // SETTINGIO_H
