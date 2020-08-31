c5a19f940f797bf6ff547bb30526c3bf22f4f471
sunchengxi <sunchengxi@uniontech.com>
2020-08-31 21:03:00 +0800

    chore: [sp2] 解决社区版自定义和配置文件兼容问题
    解决社区版自定义和配置文件兼容问题
    
    Log: 解决社区版自定义和配置文件兼容问题
    
    link: Null

--- src/common/settingio.h
+++ src/common/settingio.h
@@ -12,16 +12,15 @@ public:
     explicit SettingIO(QObject *parent = nullptr);
     static bool readIniFunc(QIODevice &device,  QSettings::SettingsMap &settingsMap);
     static bool writeIniFunc(QIODevice &device, const QSettings::SettingsMap &settingsMap);
+private:
     static QString variantToString(const QVariant &v);
     static QVariant stringToVariant(const QString &s);
     static QByteArray escapedString(const QString &src);
     static QString unescapedString(const QString &src);
-    static QString canTransfer(const QString& str);
+    static QString canTransfer(const QString &str);
     static bool iniUnescapedKey(const QByteArray &key, int from, int to, QString &result);
+public:
     static bool rewrite;
-signals:
-
-public slots:
 };
 
 #endif // SETTINGIO_H
