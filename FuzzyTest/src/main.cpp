c69486e4cddcabc25486e7b9630d9e9668f1e2d9
zhukewei <zhukewei@uniontech.com>
2020-09-22 17:18:17 +0800

    chore: 模糊测试代码上传
    模糊测试代码上传
    
    Log: 模糊测试代码上传
    
    link: Null
    Change-Id: Ia686cd7e5077e3333409205d8fbde94a36b5369d
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-terminal/+/5890
    Reviewed-by: zhangmeng <zhangmeng@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>

---
+++ FuzzyTest/src/main.cpp
@@ -0,0 +1,38 @@
+
+#include <stdint.h>
+#include <stddef.h>
+#include <atspi/atspi.h>
+#include <iostream>
+using namespace std;
+
+void on_event(AtspiEvent *event, void *data)
+{
+    g_object_unref(event->source);
+    g_free(event->type);
+    g_value_unset(&event->any_data);
+    g_free(event);
+}
+
+int init(void)
+{
+    int result = atspi_init();
+    if (result != 0)
+    {
+        std::cout <<"init error"<<std::endl;
+        return 0;
+    }
+      AtspiEventListener *listener = atspi_event_listener_new(on_event, NULL, NULL);
+      if (listener)
+      {
+          std::cout<<"listener OK"<<std::endl;
+          atspi_event_main();
+      }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    init();
+  return 0;
+}
+
+
+
