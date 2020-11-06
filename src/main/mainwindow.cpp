ac81e70dc48f5de27df7f50da5ea9e0d095ea90b
sunchengxi <sunchengxi@uniontech.com>
2020-11-06 15:51:16 +0800

    chore: [sp4] 捕获回车键完善
    Key_Enter是小键盘的确认键，Key_Return是大键盘的回车键
    
    Log: Key_Enter是小键盘的确认键，Key_Return是大键盘的回车键
    Change-Id: Iba44c010f5fec2d18b93d447eb9b569f15e35eb7

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -131,7 +131,7 @@ void SwitchThemeMenu::keyPressEvent(QKeyEvent *event)
     }
     //内置主题屏蔽 除了 上下左右回车键的其他按键响应 处理bug#53439
     if (event->key() == Qt::Key_Left || event->key() == Qt::Key_Up || event->key() == Qt::Key_Right
-            || event->key() == Qt::Key_Down || event->key() == Qt::Key_Enter) {
+            || event->key() == Qt::Key_Down || event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return) {
         return QMenu::keyPressEvent(event);
     }
 }
