1b80e410d58ef31488442d8212299a2383f0e1f3
daizhengwen <daizhengwen@uniontech.com>
2020-11-09 14:12:53 +0800

    fix: [all] bug#53752 远程管理配置文件密码显示明文
    1. 不往配置文件中写入密码
    2. 远程管理中的密码从libsecret中获取
    3. 将原本有密码的配置文件删除
    
    Log: 远程管理配置文件密码显示明文
    
    Bug: https://pms.uniontech.com/zentao/bug-view-53752.html
    Change-Id: I19f25c298c560c41595bf6bacb0a79c29fe40b47

--- src/remotemanage/serverconfigmanager.cpp
+++ src/remotemanage/serverconfigmanager.cpp
@@ -33,7 +33,8 @@ void ServerConfigManager::settServerConfig(QSettings &commandsSettings, const QS
 {
     commandsSettings.beginGroup(strGroupName);
     commandsSettings.setValue("Name", config->m_serverName);
-    commandsSettings.setValue("Password", config->m_password);
+    // 不将密码写入配置文件中
+    //commandsSettings.setValue("Password", config->m_password);
     commandsSettings.setValue("GroupName", config->m_group);
     commandsSettings.setValue("Command", config->m_command);
     commandsSettings.setValue("Path", config->m_path);
@@ -101,8 +102,8 @@ void ServerConfigManager::initServerConfig()
         serversSettings.endGroup();
 
 
-        // 兼容旧版本的操作
-        if (strList.count() == OLD_VERTIONCOUNT) {
+        // 兼容旧版本的操作 1) 旧版配置文件需要重写 2) 密码明文需要重写
+        if (strList.count() == OLD_VERTIONCOUNT || !pServerConfig->m_password.isEmpty()) {
             isConvertData = true;
         }
 
