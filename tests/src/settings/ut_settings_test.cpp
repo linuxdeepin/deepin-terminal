7efd3731f1bb1126a4cbbf1b020973ec58c29415
ch593030323 <changze@uniontech.com>
2021-08-05 10:45:34 +0800

    chore: 终端UT更新
    settings类 增加 行数覆盖率
    
    Log: 终端UT更新
    Change-Id: Ia5c2b4bf98aea317c7389e4b18430fe20bcf8c10

--- tests/src/settings/ut_settings_test.cpp
+++ tests/src/settings/ut_settings_test.cpp
@@ -19,59 +19,102 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "ut_settings_test.h"
 
+#include "ut_defines.h"
 #include "settings.h"
+#include "../stub.h"
 
 //Qt单元测试相关头文件
 #include <QTest>
 #include <QtGui>
 #include <QDebug>
 #include <QFile>
+#include <QMenu>
 
 #include <DSettings>
 #include <DSettingsOption>
+
+#include <gtest/gtest.h>
 
 DCORE_USE_NAMESPACE
 
 extern void GenerateSettingTranslate();
 
-UT_Settings_Test::UT_Settings_Test()
+
+class UT_Settings_Test : public ::testing::Test
 {
+public:
+    UT_Settings_Test() {}
+
+public:
+    //这里的几个函数都会自动调用
+
+    //用于做一些初始化操作
+    virtual void SetUp() {}
+
+    //用于做一些清理操作
+    virtual void TearDown() {}
+};
+
+static bool ut_file_exists()
+{
+    return false;
 }
 
-void UT_Settings_Test::SetUp()
+static int ut_combobox_findText(const QString &, Qt::MatchFlags)
 {
-    m_settings = Settings::instance();
+    return -1;
 }
 
-void UT_Settings_Test::TearDown()
+static int ut_stringlist_size()
 {
+    return 0;
+}
+
+static QString ut_variant_toString()
+{
+    return QString();
+}
+
+static void doDeleteLater(DSettingsOption *obj)
+{
+    obj->deleteLater();
 }
 
 #ifdef UT_SETTINGS_TEST
+
+TEST_F(UT_Settings_Test, init)
+{
+    Stub stub;
+    stub.set((QString (QVariant::*)() const)ADDR(QVariant, toString), ut_variant_toString);
+    stub.set((bool (QFile::*)() const)ADDR(QFile, exists), ut_file_exists);
+
+    Settings::releaseInstance();
+    Settings::instance();
+
+}
 TEST_F(UT_Settings_Test, SettingsTest)
 {
-    m_settings->opacity();
+    Settings::instance()->opacity();
 
-    m_settings->fontName();
+    Settings::instance()->fontName();
 
-    m_settings->fontSize();
+    Settings::instance()->fontSize();
 
     //由于编码没有保存到conf，这里只验证和默认编码相同
-    m_settings->encoding();
+    Settings::instance()->encoding();
 
-    m_settings->cursorShape();
+    Settings::instance()->cursorShape();
 
-    m_settings->cursorBlink();
+    Settings::instance()->cursorBlink();
 
-    m_settings->backgroundBlur();
+    Settings::instance()->backgroundBlur();
 
-    m_settings->colorScheme();
+    Settings::instance()->colorScheme();
 
-    m_settings->PressingScroll();
+    Settings::instance()->PressingScroll();
 
-    m_settings->OutputtingScroll();
+    Settings::instance()->OutputtingScroll();
 }
 
 TEST_F(UT_Settings_Test, GenerateSettingTranslate)
@@ -81,7 +124,7 @@ TEST_F(UT_Settings_Test, GenerateSettingTranslate)
 
 TEST_F(UT_Settings_Test, createSpinButtonHandle)
 {
-    DTK_CORE_NAMESPACE::DSettingsOption option;
+    DSettingsOption option;
     option.setData("min", QVariant(10));
     option.setData("max", QVariant(90));
     Settings::instance()->createSpinButtonHandle(&option);
@@ -89,7 +132,7 @@ TEST_F(UT_Settings_Test, createSpinButtonHandle)
 
 TEST_F(UT_Settings_Test, createCustomSliderHandle)
 {
-    DTK_CORE_NAMESPACE::DSettingsOption option;
+    DSettingsOption option;
     option.setData("min", QVariant(0));
     option.setData("max", QVariant(100));
     Settings::instance()->createCustomSliderHandle(&option);
@@ -97,15 +140,58 @@ TEST_F(UT_Settings_Test, createCustomSliderHandle)
 
 TEST_F(UT_Settings_Test, createFontComBoBoxHandle)
 {
-    DTK_CORE_NAMESPACE::DSettingsOption option;
+    Stub stub;
+    stub.set((int (QStringList::*)() const)ADDR(QStringList, size), ut_stringlist_size);
+
+    DSettingsOption option;
     Settings::instance()->createFontComBoBoxHandle(&option);
 }
 
 TEST_F(UT_Settings_Test, createShortcutEditOptionHandle)
 {
-    DTK_CORE_NAMESPACE::DSettingsOption option;
+    DSettingsOption option;
     option.setValue(QVariant("Ctrl+Shift+A"));
-    Settings::instance()->createShortcutEditOptionHandle(&option);
+    KeySequenceEdit *w = static_cast<KeySequenceEdit *>(Settings::instance()->createShortcutEditOptionHandle(&option).second);
+    if(!w)
+        return;
+
+    emit w->editingFinished(QKeySequence("Backspace"));
+    emit w->editingFinished(QKeySequence("Esc"));
+    emit w->editingFinished(QKeySequence("F4"));
+    emit w->editingFinished(QKeySequence(""));
 }
+
+TEST_F(UT_Settings_Test, createShellConfigComboxOptionHandle)
+{
+    Stub stub;
+    stub.set((bool (QFile::*)() const)ADDR(QFile, exists), ut_file_exists);
+
+    QSharedPointer<DSettingsOption> option(new DSettingsOption, doDeleteLater);
+    option->setValue(QVariant("Ctrl+Shift+A"));
+    DComboBox *w = static_cast<DComboBox *>(Settings::instance()->createShellConfigComboxOptionHandle(option.data()).second);
+    if(!w)
+        return;
+
+    const QString DEFAULT_SHELL = "$SHELL";
+    emit option->valueChanged(DEFAULT_SHELL);
+    emit option->valueChanged("sh");
+    emit w->currentTextChanged("zsh");
+}
+
+TEST_F(UT_Settings_Test, loadDefaultsWhenReinstall)
+{
+    Settings::instance()->loadDefaultsWhenReinstall();
+
+    emit Settings::instance()->settings->valueChanged("", "");
+}
+TEST_F(UT_Settings_Test, handleWidthFont)
+{
+    Stub stub;
+    stub.set((int (QComboBox::*)(const QString &, Qt::MatchFlags) const)ADDR(DComboBox, findText), ut_combobox_findText);
+
+    Settings::instance()->handleWidthFont();
+    Settings::instance()->enableControlFlow();
+}
+
 
 #endif
