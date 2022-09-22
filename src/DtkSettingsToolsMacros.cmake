88924326d17e214326949d94be6039b2544b7d50
luominjiang <luominjiang@uniontech.com>
2022-09-22 15:27:27 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- src/DtkSettingsToolsMacros.cmake
+++ src/DtkSettingsToolsMacros.cmake
@@ -1,4 +1,3 @@
-# Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd
 # SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
